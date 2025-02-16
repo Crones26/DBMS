4dbe3bc6a3113eca1c6efb4348bcc0161b2ddb45
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2025-02-16 17:19:00 +0500

    HomeWork DONE

--- DML/SQLQuery3 - COUNT Teachers for Directions.sql
+++ DML/SQLQuery3 - COUNT Teachers for Directions.sql
@@ -4,25 +4,18 @@ GO
 SELECT
 		[Направление обучения ]		=	direction_name,
 		[Количество преподавателей]	=	
-			(SELECT SUM(teachers.number_of_teachers) 
-			FROM 	(SELECT discipline_id, COUNT(teacher_id) AS number_of_teachers
-					FROM Teachers, Disciplines, TeachersDisciplinesRelation
-					WHERE		teacher		=	teacher_id
-					AND			discipline	=	discipline_id
-					GROUP  BY	discipline_id
-					) teachers, DisciplinesDirectionsRelation
-					WHERE discipline = discipline_id AND direction=direction_id
+			(SELECT COUNT(DISTINCT teacher_id) 
+			FROM Teachers, TeachersDisciplinesRelation, DisciplinesDirectionsRelation
+			WHERE Teachers.teacher_id	=	TeachersDisciplinesRelation.teacher
+			AND TeachersDisciplinesRelation.discipline	= DisciplinesDirectionsRelation.discipline
+			AND DisciplinesDirectionsRelation.direction = direction_id
 			)
 			
-			
-FROM		Directions, Disciplines, DisciplinesDirectionsRelation
-WHERE		direction		=		direction_id
-AND			discipline		=		discipline_id
---GROUP BY	direction_name
-;
+FROM Directions;
+-- GROUP BY direction_name;
 
-SELECT discipline_name, COUNT(teacher_id) AS number_of_teachers
-					FROM Teachers, Disciplines, TeachersDisciplinesRelation
-					WHERE		teacher		=	teacher_id
-					AND			discipline	=	discipline_id
-					GROUP  BY	discipline_name;
+SELECT discipline_name, COUNT(DISTINCT teacher_id) AS number_of_teachers
+FROM Teachers, Disciplines, TeachersDisciplinesRelation
+WHERE teacher	=	teacher_id
+AND discipline	=	discipline_id
+GROUP BY discipline_name;
