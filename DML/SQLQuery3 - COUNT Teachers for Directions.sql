0fc5ea3be9d93f4acc014d6e15970b9d47c76052
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2025-02-17 21:34:09 +0500

    Shedule in progress

--- DML/SQLQuery3 - COUNT Teachers for Directions.sql
+++ DML/SQLQuery3 - COUNT Teachers for Directions.sql
@@ -6,16 +6,17 @@ SELECT
 		[Количество преподавателей]	=	
 			(SELECT COUNT(DISTINCT teacher_id) 
 			FROM Teachers, TeachersDisciplinesRelation, DisciplinesDirectionsRelation
-			WHERE Teachers.teacher_id	=	TeachersDisciplinesRelation.teacher
-			AND TeachersDisciplinesRelation.discipline	= DisciplinesDirectionsRelation.discipline
+			WHERE Teachers.teacher_id = TeachersDisciplinesRelation.teacher
+			AND TeachersDisciplinesRelation.discipline = DisciplinesDirectionsRelation.discipline
 			AND DisciplinesDirectionsRelation.direction = direction_id
 			)
 			
 FROM Directions;
 -- GROUP BY direction_name;
 
-SELECT discipline_name, COUNT(DISTINCT teacher_id) AS number_of_teachers
-FROM Teachers, Disciplines, TeachersDisciplinesRelation
-WHERE teacher	=	teacher_id
-AND discipline	=	discipline_id
+SELECT 
+    discipline_name			AS [Дисциплина], 
+    COUNT(DISTINCT teacher) AS [Количество преподавателей]
+FROM TeachersDisciplinesRelation, Disciplines
+WHERE discipline = discipline_id
 GROUP BY discipline_name;
---
+++ T-SQL/SQLQuery1 - INSERT PV_319.sql
@@ -0,0 +1,10 @@
+USE PV_319_Import;
+GO
+
+--DELETE FROM Schedule;
+
+INSERT	Groups
+		(group_id, group_name, direction)
+VALUES	(319,		N'PV_319',	1);
+
+SELECT * FROM Groups;
