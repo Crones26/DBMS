c51c4e619e8abe56f0057541615211e020450816
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2025-03-11 03:36:09 +0500

    ClassWork DONE

---
+++ DDL/SQLQuery7 - CREATE DaysOFF.sql
@@ -0,0 +1,10 @@
+USE PV_319_Import;
+GO
+
+DROP TABLE DaysOFF;
+
+CREATE TABLE DaysOFF
+(
+	[date]	DATE	PRIMARY KEY,
+	holiday	TINYINT	NOT NULL	CONSTRAINT FK_DO_Holidays REFERENCES Holidays(holiday_id)
+);
---
+++ T-SQL/SQLQuery0 - Holidays test.sql
@@ -0,0 +1,13 @@
+USE PV_319_Import;
+SET DATEFIRST 1;
+GO
+
+--SELECT * FROM DaysOFF;
+
+--EXEC sp_AddAllHolidaysFor 2023;
+
+SELECT
+		[Дата]		=	[date],
+		[Праздник]	=	holiday_name
+FROM	DaysOFF, Holidays
+WHERE	holiday=holiday_id;
